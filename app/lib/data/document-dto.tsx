9a4a9e7a9b4e746ce1977872b4d74af694a9babb
Philip Jurke <pjurke@icloud.com>
2024-10-04 00:10:55 +0200

    harden document-dto

--- app/lib/data/document-dto.tsx
+++ app/lib/data/document-dto.tsx
@@ -7,9 +7,14 @@ import { ObjectId } from 'mongodb';
 
 export async function getDocument(id: ObjectId): Promise<Document | null> {
 
+    // To do: If the id is not defined or not valid, then throw an exception
+    if (!id || !ObjectId.isValid(id)) {
+        throw new Error('Invalid document ID');
+    }
+
     try {
         const client = await clientPromise;
-        const db = client.db(env.DB_NAME || 'timeline');
+        const db = client.db(env.DB_NAME || 'text-review-db');
 
         const document = await db
             .collection<Document>('documents')
@@ -17,10 +22,16 @@ export async function getDocument(id: ObjectId): Promise<Document | null> {
                 _id: id
             });
 
+        // To do: Error message in case document has not been found.
+        if (!document) {
+            console.log(`Document with ID ${id} not found.`);
+            return null;
+        }
+
         return document;
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log('Error retrieving document:', error);
+        throw new Error('Failed to retrieve document');
     }
 
 }
