a1b2beac65463f27169338a249a879ac61bdb121
Philip Jurke <pjurke@icloud.com>
2024-10-04 15:58:19 +0200

    improve readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Text Review"
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: ReactNode; }>) {
   return (
     <html lang="en">
       <body>
