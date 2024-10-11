ce06fd075edc675918967b96850a6d43c01b6d9d
Philip Jurke <pjurke@icloud.com>
2024-10-11 19:44:58 +0200

    change headline weight

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="flex flex-col h-screen">
       <div className="flex-grow flex items-center justify-center flex-col px-4 text-center">
-        <h1 className="text-5xl font-bold text-gray-800 mb-4">Text Review</h1>
+        <h1 className="font-light text-5xl text-gray-800 mb-4">Text Review</h1>
       </div>
     </section>
   );
