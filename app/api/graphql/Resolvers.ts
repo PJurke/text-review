e95e8e0bc92123b8443b77a1f93a400c78e02d5f
Philip Jurke <pjurke@icloud.com>
2024-12-12 17:39:02 +0100

    enrich resolver

--- app/api/graphql/Resolvers.ts
+++ app/api/graphql/Resolvers.ts
@@ -1,12 +1,19 @@
 import { getDocument } from "@/app/lib/data/document-dto";
+import { GraphQLResolveInfo } from "graphql";
+
+interface TextDocumentArgs {
+    id: string
+}
 
 export const resolvers = {
     Query: {
-        TextDocument: async (_: any, args: any) => {
+
+        TextDocument: async (_parent: unknown, args: TextDocumentArgs, context: any, _info: GraphQLResolveInfo) => {
 
             const { id } = args
             return getDocument(id);
 
         }
+
     }
 }
