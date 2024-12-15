7278ee7428fb59bf75b657506a858009571773f4
Philip Jurke <pjurke@icloud.com>
2024-12-15 14:36:07 +0100

    refine highlight validation

--- types/Highlight.ts
+++ types/Highlight.ts
@@ -11,5 +11,5 @@ export const HighlightSchema = z.object({
     id: z.string().uuid(),
     paragraphId: z.string().uuid(),
     start: z.number().int().nonnegative('The start position must be a positive integer'),
-    end: z.number().int()
+    end: z.number().int().nonnegative('The end position must be a positive integer')
 });
