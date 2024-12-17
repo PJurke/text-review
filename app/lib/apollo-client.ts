6559150eb0380f721aea7763fcce038eed8f4470
Philip Jurke <pjurke@icloud.com>
2024-12-17 16:11:23 +0100

    use gql env variable

--- app/lib/apollo-client.ts
+++ app/lib/apollo-client.ts
@@ -1,10 +1,16 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { env } from "process";
 
 const createApolloClient = () => {
+
+    if (!env.GRAPHQL_ENDPOINT)
+        throw new Error("Please add the GRAPHQL_ENDPOINT variable to your environmental variables.");
+
     return new ApolloClient({
-        uri: 'https://localhost:3000/api/graphql',
+        uri: env.GRAPHQL_ENDPOINT,
         cache: new InMemoryCache()
     });
+    
 };
 
 export default createApolloClient;
