d5cb99775dd4f1af010f68cbfb2f80ac24fefadf
Philip Jurke <pjurke@icloud.com>
2024-12-26 23:40:17 +0100

    allow provisional cors

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { ApolloServer } from "@apollo/server";
 import { readFileSync } from "fs";
 import { resolvers } from "./resolvers";
@@ -24,4 +24,70 @@ const handler = startServerAndCreateNextHandler<NextRequest>(apolloServer, {
     context: async req => ({ req }),
 });
 
-export { handler as GET, handler as POST };
+const ALLOWED_ORIGINS = [
+    'http://192.168.0.12:3000',
+    'http://localhost:3000',
+]
+
+const getAllowedOrigin = (origin: string | null): string | null => {
+    if (origin && ALLOWED_ORIGINS.includes(origin))
+        return origin;
+    return null;
+};
+
+const setCORSHeaders = (response: Response, origin: string | null) => {
+    if (origin) {
+        response.headers.set('Access-Control-Allow-Origin', origin);
+        response.headers.set('Access-Control-Allow-Credentials', 'true');
+    }
+};
+
+export const GET = async (req: NextRequest) => {
+    const origin = req.headers.get('origin');
+    const allowedOrigin = getAllowedOrigin(origin);
+
+    if (req.method === 'OPTIONS') {
+        if (allowedOrigin) {
+            return new NextResponse(null, {
+                status: 204,
+                headers: {
+                    'Access-Control-Allow-Origin': allowedOrigin,
+                    'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                    'Access-Control-Max-Age': '86400', // 24 Stunden
+                },
+            });
+        } else {
+            return new NextResponse(null, { status: 403 }); // Forbidden
+        }
+    }
+
+    const response = await handler(req);
+    setCORSHeaders(response, allowedOrigin);
+    return response;
+};
+
+export const POST = async (req: NextRequest) => {
+    const origin = req.headers.get('origin');
+    const allowedOrigin = getAllowedOrigin(origin);
+
+    if (req.method === 'OPTIONS') {
+        if (allowedOrigin) {
+            return new NextResponse(null, {
+                status: 204,
+                headers: {
+                    'Access-Control-Allow-Origin': allowedOrigin,
+                    'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                    'Access-Control-Max-Age': '86400', // 24 Stunden
+                },
+            });
+        } else {
+            return new NextResponse(null, { status: 403 }); // Forbidden
+        }
+    }
+
+    const response = await handler(req);
+    setCORSHeaders(response, allowedOrigin);
+    return response;
+};
