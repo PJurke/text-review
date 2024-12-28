939ace4ef7433c79857f593106c00dcf31c94711
Philip Jurke <pjurke@icloud.com>
2024-12-28 22:40:45 +0100

    add internal server error

---
+++ services/shared/errors/InternalServerError.ts
@@ -0,0 +1,9 @@
+
+export class InternalServerError extends Error {
+
+    constructor(message: string = 'Internal Server Error') {
+        super(message);
+        this.name = 'InternalServerError';
+    }
+
+}
