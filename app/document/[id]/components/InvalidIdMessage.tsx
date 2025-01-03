eb7357975afe5ef69de383e7295a6269e8c77512
Philip Jurke <pjurke@icloud.com>
2025-01-03 19:48:07 +0100

    add type to component

--- app/document/[id]/components/InvalidIdMessage.tsx
+++ app/document/[id]/components/InvalidIdMessage.tsx
@@ -1,18 +1,20 @@
 import UserMessageComponent from "@/components/UserMessage";
 
-export default function InvalidIdMessage() {
+export default function InvalidIdMessage(): JSX.Element {
     
-    return <UserMessageComponent
-        icon={{
-            alt: 'ID Invalid',
-            url: '/id-invalid.png'
-        }}
-        title='Document ID invalid'
-        message='Each document is identified by a unique id. The given document id is invalid.'
-        action={{
-            label: 'Search for Documents',
-            url: 'https://www.google.de'
-        }}
-    />
+    return (
+        <UserMessageComponent
+            icon={{
+                alt: 'ID Invalid',
+                url: '/id-invalid.png'
+            }}
+            title='Document ID invalid'
+            message='Each document is identified by a unique id. The given document id is invalid.'
+            action={{
+                label: 'Search for Documents',
+                url: '/'
+            }}
+        />
+    );
     
 }
