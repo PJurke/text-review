f7d01206914cfb50ca85a00d6b9ca9d3aac73784
Philip Jurke <pjurke@icloud.com>
2025-01-03 19:47:15 +0100

    add type to component

--- app/document/[id]/page.tsx
+++ app/document/[id]/page.tsx
@@ -1,6 +1,6 @@
 import TextDocumentComponent from './components/TextDocumentComponent';
 
-export default async function Page() {
+export default function Page(): JSX.Element {
     return (
         <section className="max-w-[50ch] md:max-w-[75ch] mx-auto p-4 text-wrap transition-[max-width]">
             <TextDocumentComponent />
