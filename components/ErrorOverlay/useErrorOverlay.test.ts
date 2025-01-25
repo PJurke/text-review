bc4f42e47864f25440e9abda9eaa6db499187c8c
Philip Jurke <pjurke@icloud.com>
2025-01-26 00:41:48 +0100

    add more tests

--- components/ErrorOverlay/useErrorOverlay.test.ts
+++ components/ErrorOverlay/useErrorOverlay.test.ts
@@ -4,40 +4,29 @@ import { act } from 'react';
 
 describe('useErrorOverlay Store', () => {
 
+    const initialContent: ErrorOverlayContent = {
+        title: '',
+        message: ''
+    };
+
     // Reset store before every new test
     beforeEach(() => {
-        const { setErrorOverlayContent, hideErrorOverlay } = useErrorOverlay.getState();
-
-        setErrorOverlayContent({
-            title: '',
-            message: '',
-            action: undefined,
+        act(() => {
+            useErrorOverlay.getState().setErrorOverlayContent(initialContent);
+            useErrorOverlay.getState().hideErrorOverlay();
         });
-
-        hideErrorOverlay();
     });
 
     it('should set the initial state correctly', () => {
         const state = useErrorOverlay.getState();
-
-        expect(state.errorOverlayContent).toEqual({
-            title: '',
-            message: '',
-            action: undefined,
-        });
-
+        expect(state.errorOverlayContent).toEqual(initialContent);
         expect(state.isErrorOverlayVisible).toBe(false);
     });
 
-    it('should set the ErrorOverlayContent correctly', () => {
-
+    it('should set ErrorOverlayContent without an action', () => {
         const newContent: ErrorOverlayContent = {
-            title: 'Error',
-            message: 'An unexpected error occurred.',
-            action: {
-                label: 'Retry',
-                handler: () => {}
-            }
+            title: 'Fehler',
+            message: 'Ein unerwarteter Fehler ist aufgetreten.',
         };
 
         act(() => {
@@ -45,16 +34,37 @@ describe('useErrorOverlay Store', () => {
         });
 
         const state = useErrorOverlay.getState();
-
         expect(state.errorOverlayContent).toEqual(newContent);
+        expect(state.errorOverlayContent.action).toBeUndefined();
     });
 
-    it('should make Error Overlay visible', () => {
+    it('should set ErrorOverlayContent with an action', () => {
+        const mockHandler = jest.fn();
 
-        const errorMessage = {
+        const newContent: ErrorOverlayContent = {
             title: 'Error',
-            message: 'An unexpected error occurred',
-            action: undefined,
+            message: 'An unexpected error occurred.',
+            action: {
+                label: 'Retry',
+                handler: mockHandler,
+            },
+        };
+
+        act(() => {
+            useErrorOverlay.getState().setErrorOverlayContent(newContent);
+        });
+
+        const state = useErrorOverlay.getState();
+        expect(state.errorOverlayContent).toEqual(newContent);
+        expect(state.errorOverlayContent.action).toBeDefined();
+        expect(state.errorOverlayContent.action?.label).toBe('Retry');
+        expect(typeof state.errorOverlayContent.action?.handler).toBe('function');
+    });
+
+    it('should make Error Overlay become visible without a set action', () => {
+        const errorMessage: ErrorOverlayContent = {
+            title: 'Error',
+            message: 'An unexpected error occurred.',
         };
 
         act(() => {
@@ -66,14 +76,36 @@ describe('useErrorOverlay Store', () => {
         expect(state.isErrorOverlayVisible).toBe(true);
     });
 
+    it('should make Error Overlay become visible, with a set action', () => {
+        const mockHandler = jest.fn();
+
+        const errorMessage: ErrorOverlayContent = {
+            title: 'Error',
+            message: 'An unexpected error occurred.',
+            action: {
+                label: 'Retry',
+                handler: mockHandler,
+            },
+        };
+
+        act(() => {
+            useErrorOverlay.getState().showErrorOverlay(errorMessage);
+        });
+
+        const state = useErrorOverlay.getState();
+        expect(state.errorOverlayContent).toEqual(errorMessage);
+        expect(state.isErrorOverlayVisible).toBe(true);
+        expect(state.errorOverlayContent.action).toBeDefined();
+        expect(state.errorOverlayContent.action?.label).toBe('Retry');
+    });
+
     it('should hide Error Overlay', () => {
 
         // Show Error Overlay first
         act(() => {
             useErrorOverlay.getState().showErrorOverlay({
               title: 'Error',
-              message: 'An unexpected error occurred',
-              action: undefined,
+              message: 'An unexpected error occurred'
             });
         });
 
@@ -84,6 +116,28 @@ describe('useErrorOverlay Store', () => {
 
         const state = useErrorOverlay.getState();
         expect(state.isErrorOverlayVisible).toBe(false);
+    });
+
+    it('should call the action handler function', () => {
+        const mockHandler = jest.fn();
+
+        const errorMessage: ErrorOverlayContent = {
+            title: 'Error',
+            message: 'An unexpected error occurred.',
+            action: {
+              label: 'Retry',
+              handler: mockHandler,
+            },
+        };
+
+        act(() => {
+            useErrorOverlay.getState().showErrorOverlay(errorMessage);
+        });
+
+        const state = useErrorOverlay.getState();
+        state.errorOverlayContent.action?.handler();
+
+        expect(mockHandler).toHaveBeenCalledTimes(1);
     });
 
 });
