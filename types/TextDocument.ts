b93cc137d4dddc5b51072d8e07da2e3615e00b37
Philip Jurke <pjurke@icloud.com>
2025-01-26 19:18:00 +0100

    add to types

--- types/TextDocument.ts
+++ types/TextDocument.ts
@@ -5,6 +5,7 @@ import Paragraph, { ParagraphSchema } from "./Paragraph"
 export default interface TextDocument {
     id: string
     title: string
+    author: string
     paragraphs: Paragraph[]
     highlights: Highlight[]
 }
@@ -12,6 +13,7 @@ export default interface TextDocument {
 export const TextDocumentSchema = z.object({
     id: z.string().refine((value) => /^[0-9a-fA-F]{24}$/.test(value), 'The id of a text document must be a valid object id'),
     title: z.string(),
+    author: z.string(),
     paragraphs: z.array(ParagraphSchema),
     highlights: z.array(HighlightSchema)
 })
