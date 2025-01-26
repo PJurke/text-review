6256332ca922640396fb93460ed1ec45b797078e
Philip Jurke <pjurke@icloud.com>
2025-01-26 16:02:46 +0100

    add loading skeleton

---
+++ app/document/[id]/_components/LoadingSkeleton.tsx
@@ -0,0 +1,18 @@
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+export default function LoadingSkeleton()  {
+    return (
+        <div>
+            <Skeleton count={1} height={36} />
+            <div className='mt-8'></div>
+            <Skeleton count={2} height={30} />
+            <div className='mt-8'></div>
+            <Skeleton count={4} height={30} />
+            <div className='mt-8'></div>
+            <Skeleton count={6} height={30} />
+            <div className='mt-8'></div>
+            <Skeleton count={6} height={30} />
+        </div>
+    );
+}
