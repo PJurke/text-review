9496e9acd03295ffec1531e3ab6d5bb187d4eada
Philip Jurke <pjurke@icloud.com>
2025-02-19 21:12:17 +0100

    adjust author color

--- app/document/_components/TextDocumentItem.tsx
+++ app/document/_components/TextDocumentItem.tsx
@@ -10,7 +10,7 @@ export default function TextDocumentItem({ id, title, author }: TextDocumentSumm
                 <h3 className="text-xl">{title}</h3>
 
                 <Show when={!!author}>
-                    <div className="text-gray-600 text-sm">by {author}</div>
+                    <div className="text-neutral-500 text-sm">by {author}</div>
                 </Show>
 
             </a>
