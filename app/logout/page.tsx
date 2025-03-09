2bced443536c364c81e9b311b48f1b27f21dad3a
Philip Jurke <pjurke@icloud.com>
2025-03-09 22:18:39 +0100

    add basic logout page (later profile page)

---
+++ app/logout/page.tsx
@@ -0,0 +1,12 @@
+import LogoutComponent from "@/components/auth/LogoutComponent";
+
+export default function Page(): JSX.Element {
+
+    return (
+        <main className="max-w-[50ch] md:max-w-[75ch] mx-auto p-4 text-wrap transition-[max-width]">
+            <h1 className="text-3xl">Logout</h1>
+            <LogoutComponent />
+        </main>
+    );
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <section className="flex flex-col h-screen">
       <div className="grow flex items-center justify-center flex-col px-4 text-center">
         <h1 className="font-light text-5xl text-gray-800 mb-4">Text Review</h1>
+        <Link href="/login">Log in</Link>
+        <Link href="/logout">Log out</Link>
       </div>
     </section>
   );
