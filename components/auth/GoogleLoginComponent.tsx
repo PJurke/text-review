cb4d2ab4029446c1b6322e95f0d3c33e38b91929
Philip Jurke <pjurke@icloud.com>
2025-03-10 20:00:07 +0100

    design google login button

--- components/auth/GoogleLoginComponent.tsx
+++ components/auth/GoogleLoginComponent.tsx
@@ -1,13 +1,18 @@
 import { signIn } from "@/lib/auth";
 
 export default function GoogleLoginComponent(): JSX.Element {
+
+    const signInWithGoogle = async () => {
+        "use server"
+        await signIn("google", { redirectTo: "/user"})
+    }
+
     return (
 
-        <form action={async () => {
-            "use server"
-            await signIn("google", { redirectTo: "/user"})}}
-        >
-            <button className="cursor-pointer" type="submit">Signin with Google</button>
+        <form action={signInWithGoogle} className="flex justify-center">
+            <button className="bg-blue-600 hover:bg-blue-700 cursor-pointer font-bold mt-8 px-4 py-2 rounded text-white w-full max-w-xs" type="submit">
+                Sign in with Google
+            </button>
         </form>
 
     );
