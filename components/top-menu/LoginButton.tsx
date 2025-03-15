d31761cc7d5ed56703a708d579d93c8c0665d31a
Philip Jurke <pjurke@icloud.com>
2025-03-15 22:55:46 +0100

    add login button with session mgmt

---
+++ components/top-menu/LoginButton.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/lib/auth";
+
+export default async function LoginButton() {
+    
+    const session = await auth();
+
+    if (!session?.user) {
+        return (
+            <a className="block p-3 hover:bg-gray-100" href="/login">Log in</a>
+        );
+    }
+
+    return (
+        <a className="block p-3 hover:bg-gray-100" href="/user">{ session.user.name }</a>
+    );
+}
--- components/top-menu/TopMenu.tsx
+++ components/top-menu/TopMenu.tsx
@@ -1,11 +1,13 @@
+import LoginButton from "./LoginButton";
+
 export default function TopMenu(): JSX.Element {
 
     return (
         <nav aria-label="Main Navigation" className="bg-white fixed w-full">
             <ul className="flex flex-col sm:flex-row sm:justify-center">
                 <li><a className="block p-3 hover:bg-gray-100" href="/">Homepage</a></li>
-                <li><a className="block p-3 hover:bg-gray-100" href="/document">Texts</a></li>
-                <li><a className="block p-3 hover:bg-gray-100" href="/login">Log in</a></li>
+                <li><a className="block p-3 hover:bg-gray-100" href="/document">Documents</a></li>
+                <li><LoginButton /></li>
             </ul>
         </nav>
     );
