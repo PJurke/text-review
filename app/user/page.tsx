ebc5fb6dc8a5a58e3f3c7210137ae9669520c618
Philip Jurke <pjurke@icloud.com>
2025-03-15 23:15:39 +0100

    link to google acc mgmt

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -11,21 +11,22 @@ export default async function Page(): Promise<JSX.Element> {
 
     const session = await auth();
 
-    if (!session) redirect('/login');
-    if (!session?.user) return <></>;
+    if (!session?.user) redirect('/login');
+
+    const user = session.user;
 
     return (
 
         <main className="max-w-[50ch] md:max-w-[75ch] mx-auto p-4">
             <h1 className="text-3xl">User</h1>
 
-            { session.user.image &&
-                <img alt="My Profile Picture" className="border-8 border-gray-200 mt-10 mx-auto rounded-xl w-40" src={session.user.image} />
+            { user.image &&
+                <img alt="My Profile Picture" className="border-8 border-gray-200 mt-10 mx-auto rounded-xl w-40" src={user.image} />
             }
 
-            <div aria-label="User Name" className="mt-4 text-center text-2xl">{session.user.name!}</div>
-
-            <div aria-label="Email address" className="text-center text-gray-500">{session.user.email}</div>
+            <div aria-label="User Name" className="mt-4 text-center text-2xl">{user.name!}</div>
+            <div aria-label="Email address" className="text-center text-gray-500">{user.email}</div>
+            <a className="text-center underline" href="https://myaccount.google.com" target="_blank">Manage your profile on Google</a>
             
             <LogoutButton className="mt-4" />
         </main>
