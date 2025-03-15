9563c6b4f82b3530fffcb81e57890472cc5edded
Philip Jurke <pjurke@icloud.com>
2025-03-15 23:25:08 +0100

    top menu goes sticky

--- components/top-menu/TopMenu.tsx
+++ components/top-menu/TopMenu.tsx
@@ -3,7 +3,7 @@ import LoginMenuItem from "./LoginMenuItem";
 export default function TopMenu(): JSX.Element {
 
     return (
-        <nav aria-label="Main Navigation" className="backdrop-blur-md bg-white/75 fixed w-full">
+        <nav aria-label="Main Navigation" className="backdrop-blur-md bg-white/75 sticky top-0 w-full">
             <ul className="flex flex-col sm:flex-row sm:justify-center">
                 <li><a className="block p-3" href="/">Homepage</a></li>
                 <li><a className="block p-3" href="/document">Documents</a></li>
