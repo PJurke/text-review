988041ec8801c9262206c34ddba3025f21159351
Philip Jurke <pjurke@icloud.com>
2025-03-16 13:29:12 +0100

    make Google profile pictures look sharp

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
+import Image from 'next/image'
 import { auth } from "@/lib/auth";
 import LogoutButton from "@/components/auth/LogoutButton";
 
@@ -7,13 +8,17 @@ export const metadata: Metadata = {
     title: 'Text Review | User'
 }
 
+const getGoogleProfileImageUrl = (url: string, size: number = 200): string => {
+    return url.replace(/s\d+-c/, `s${size}-c`);
+};
+
 export default async function Page(): Promise<JSX.Element> {
 
     const session = await auth();
 
     if (!session?.user) redirect('/login');
 
-    const user = session.user;
+    const { user } = session;
 
     return (
 
@@ -21,7 +26,7 @@ export default async function Page(): Promise<JSX.Element> {
             <h1 className="text-3xl">User</h1>
 
             { user.image &&
-                <img alt="My Profile Picture" className="border-8 border-gray-200 mt-10 mx-auto rounded-xl w-40" src={user.image} />
+                <Image alt="My Profile Picture" className="border-8 border-gray-200 mt-10 mx-auto rounded-xl" height={200} src={getGoogleProfileImageUrl(user.image)} width={200} />
             }
 
             <div aria-label="User Name" className="mt-4 text-center text-2xl">{user.name!}</div>
