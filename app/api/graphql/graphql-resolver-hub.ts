6f69dade89684ace788e2de104086ca1dcd20d2e
Philip Jurke <pjurke@icloud.com>
2025-03-18 19:56:00 +0100

    deactivate future features

--- app/api/graphql/graphql-resolver-hub.ts
+++ app/api/graphql/graphql-resolver-hub.ts
@@ -1,27 +1,27 @@
 import { IResolvers } from '@graphql-tools/utils';
 
-import getTextAnalysisResolver from '@/services/get-text-analysis/graphql/get-text-analysis-resolver';
+//import getTextAnalysisResolver from '@/services/get-text-analysis/graphql/get-text-analysis-resolver';
 import getTextDocumentResolver from '@/services/get-text-document/graphql/get-text-document-resolver';
 
-import createTextAnalysis from '@/services/create-text-analysis/graphql/create-text-analysis-resolver';
-import addHighlightResolver from "@/services/add-highlight/graphql/add-highlight-resolver";
-import removeHighlightResolver from "@/services/remove-highlight/graphql/remove-highlight-resolver";
+//import createTextAnalysis from '@/services/create-text-analysis/graphql/create-text-analysis-resolver';
+//import addHighlightResolver from "@/services/add-highlight/graphql/add-highlight-resolver";
+//import removeHighlightResolver from "@/services/remove-highlight/graphql/remove-highlight-resolver";
 import listTextDocumentsResolver from '@/services/list-text-documents/graphql/list-text-documents-resolver';
 
 export const resolvers: IResolvers = {
     Query: {
 
-        textAnalysis: getTextAnalysisResolver,
+        //textAnalysis: getTextAnalysisResolver,
         
         textDocument: getTextDocumentResolver,
         textDocuments: listTextDocumentsResolver
 
     },
-    Mutation: {
+    /*Mutation: {
 
-        addHighlight: addHighlightResolver,
-        createTextAnalysis: createTextAnalysis,
-        removeHighlight: removeHighlightResolver
+        //addHighlight: addHighlightResolver,
+        //createTextAnalysis: createTextAnalysis,
+        //removeHighlight: removeHighlightResolver
 
-    }
+    }*/
 }
