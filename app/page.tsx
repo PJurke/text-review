e249911b4ee2384742c962907ae60ede32ecedcf
Philip Jurke <pjurke@icloud.com>
2025-04-04 18:24:42 +0200

    add grid, adapt hero and purpose sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,28 @@ import FunctionCard from "./_components/FunctionCard";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen">
 
-      <section className="grow flex items-center justify-center flex-col px-4 text-center">
-        <h1 className="font-light text-5xl text-gray-800 mb-2">Text Review</h1>
-        <p className="text-lg">Language shapes our interactions. Texts carry meaning.</p>
+    <div className="grid grid-cols-12 gap-4">
+
+      { /* Hero Section */ }
+      <section className="col-span-12 flex flex-col gap-4 items-center justify-center min-h-[70vh] text-center text-gray-800">
+        <h1 className="font-light text-5xl">Text Review</h1>
+        <p className="leading-relaxed text-xl">
+          Language shapes our interactions.<br />
+          Texts carry meaning.</p>
       </section>
 
-      <section>
-        <p className="bg-gray-100 px-2 py-16 text-center text-xl">
-          Those who understand can question. <br className="hidden sm:inline" />
-          Those who question can really help shape things. <br className="hidden sm:inline" />
-          I have developed Text Review to help you do this.
+      { /* Purpose Section */ }
+      <section className="bg-blue-50 col-span-12 grid grid-cols-subgrid items-center min-h-[50vh] text-gray-800">
+        <p className="col-span-12 leading-relaxed text-center text-xl">
+          Those who understand can question.<br />
+          Those who question can really help shape things.<br />
+          I have developed Text Review to help you do this.<br />
         </p>
       </section>
 
-      <section>
+      { /* Feature Section */ }
+      <section className="col-span-12">
         <div>
           <FunctionCard image={{ alt: "", src: "/hamburger-icon.svg" }} title="Offizielle Texte" description="Wir holen Texte von offiziellen Quellen und sammeln sie hier." />
           <FunctionCard image={{ alt: "", src: "/hamburger-icon.svg" }} title="Abschnitte hervorheben" description="Hebe für dich wichtige Textstellen hervor." />
@@ -26,5 +32,6 @@ export default function Home() {
       </section>
 
     </div>
+
   );
 }
