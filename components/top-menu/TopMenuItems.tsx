abe7779d8a6a53ae2f54d3b228eaf8172c6f7305
Philip Jurke <pjurke@icloud.com>
2025-05-08 21:32:34 +0200

    link why to top menu

--- components/top-menu/TopMenuItems.tsx
+++ components/top-menu/TopMenuItems.tsx
@@ -8,6 +8,7 @@ export default function TopMenuItems({ isOpen }: TopMenuItemsProps): JSX.Element
     return (
         <ul className={`flex flex-col sm:flex-row items-center sm:justify-center gap-x-4 sm:flex ${isOpen ? 'block' : 'hidden'}`}>
             <TopMenuItem href="/">Homepage</TopMenuItem>
+            <TopMenuItem href="/why">Why</TopMenuItem>
             <TopMenuItem href="/document">Documents</TopMenuItem>
         </ul>
     );
