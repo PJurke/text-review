ea8c94bc4f3edc78b72d7cdfc3228f6b7622c397
Philip Jurke <pjurke@icloud.com>
2025-05-22 16:36:21 +0200

    top menu outside click

--- components/top-menu/TopMenu.tsx
+++ components/top-menu/TopMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import TopMenuToggleButton from "./TopMenuToggleButton";
 import TopMenuItems from "./TopMenuItems";
 
@@ -9,11 +9,32 @@ import TopMenuItems from "./TopMenuItems";
  */
 export default function TopMenu(): JSX.Element {
 
+    const menuRef = useRef<HTMLElement>(null);
+
     const [isOpen, setIsOpen] = useState(false);
-    const toggleMenu = () => setIsOpen(prev => !prev);
+    const toggleMenu = useCallback(() => {
+        setIsOpen(prev => !prev);
+    }, []);
+
+    const handleClickOutside = useCallback((event: MouseEvent) => {
+        if (isOpen && menuRef.current && !menuRef.current.contains(event.target as Node)) {
+            setIsOpen(false);
+        }
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (isOpen)
+            document.addEventListener('mousedown', handleClickOutside);
+        else
+            document.removeEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isOpen, handleClickOutside]);
 
     return (
-        <nav aria-label="Main Navigation" className="backdrop-blur-md bg-white/75 flex flex-col sm:flex-row justify-center gap-x-4 sticky top-0">
+        <nav aria-label="Main Navigation" className="backdrop-blur-md bg-white/75 flex flex-col sm:flex-row justify-center gap-x-4 sticky top-0" ref={menuRef}>
 
             <TopMenuToggleButton isOpen={isOpen} toggleMenu={toggleMenu} />
             <TopMenuItems isOpen={isOpen} />
