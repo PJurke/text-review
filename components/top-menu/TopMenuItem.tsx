50432707131b4d7b3b9e8d06edef30d583256643
Philip Jurke <pjurke@icloud.com>
2025-05-22 16:17:22 +0200

    add active item state

--- components/top-menu/TopMenuItem.tsx
+++ components/top-menu/TopMenuItem.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface TopMenuItemProps {
     href: string;
@@ -9,5 +12,16 @@ interface TopMenuItemProps {
  * Represents a single menu item as a link.
  */
 export default function TopMenuItem({ children, href }: TopMenuItemProps): JSX.Element {
-    return <li><Link className="block font-medium px-3 py-2" href={href}>{children}</Link></li>
+
+    const pathname = usePathname();
+    const isActive = pathname === href;
+
+    const cssClasses = `block font-medium px-3 py-2 ${isActive ? 'font-semibold' : ''}`;
+
+    return (
+        <li>
+            <Link className={cssClasses} href={href}>{children}</Link>
+        </li>
+    );
+
 }
