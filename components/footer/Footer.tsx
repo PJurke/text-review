d068d7c436b06fe1fb2eabf3cf9fdf10946255bb
Philip Jurke <pjurke@icloud.com>
2025-05-22 20:39:43 +0200

    add footer data

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Footer(): JSX.Element {
     return (
-        <footer className="bg-gray-900 flex min-h-16 items-center justify-center text-sm w-full">
-            <div className="text-white">Created by Philip Jurke</div>
+        <footer className="bg-gray-900 flex min-h-20 items-center justify-center text-center text-sm text-white w-full">
+            <p className="leading-relaxed">
+                <Link className="underline" href="/">Text Review</Link> {new Date().getFullYear()}<br />
+                Created by <Link className="underline" href="https://www.philipjurke.de" target="_blank">Philip Jurke</Link>
+            </p>
         </footer>
     );
 }
