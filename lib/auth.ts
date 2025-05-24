56b7e1bbcae7e249bdbff063f691f35ff6dcdc4e
Philip Jurke <pjurke@icloud.com>
2025-05-24 22:18:46 +0200

    add email provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,37 @@
-import NextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: []
+    providers: [
+        Credentials({
+            credentials: {
+                email: {},
+                password: {}
+            },
+            authorize: async (credentials) => {
+                
+                let user: User | null = null;
+
+                // Todo: Add salting and hashing logic
+                const hashedPassword = credentials.password;
+
+                // Todo: Check against database
+                user = {
+                    id: "0",
+                    name: "John Doe",
+                    email: "john.doe@example.com",
+                    image: "https://example.com/johndoe.jpg"
+                };
+
+                // Todo: Good error message
+                // Todo: Potentially for user registration
+                if (!user) {
+                    throw new Error("Invalid credentials.")
+                }
+
+                return user;
+
+            },
+        })
+    ]
 });
