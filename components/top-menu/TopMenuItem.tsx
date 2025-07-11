d959b3f055c523eaec074793b385b05693f621fa
Philip Jurke <pjurke@icloud.com>
2025-07-11 16:38:38 +0200

    style adjustments

--- components/top-menu/TopMenuItem.tsx
+++ components/top-menu/TopMenuItem.tsx
@@ -19,7 +19,7 @@ export default function TopMenuItem({ children, href, onClick }: TopMenuItemProp
 
     return (
         <li className="w-full sm:w-auto">
-            <Link className={`block font-medium px-3 py-4 sm:py-2 text-center w-full sm:w-auto ${isActive ? 'font-semibold' : ''}`} href={href} onClick={onClick}>
+            <Link className={`block font-medium px-3 py-1 sm:py-2 text-center w-full sm:w-auto ${isActive ? 'font-semibold text-slate-500' : ''}`} href={href} onClick={onClick}>
                 {children}
             </Link>
         </li>
--- components/top-menu/TopMenuItems.tsx
+++ components/top-menu/TopMenuItems.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import TopMenuItem from "./TopMenuItem";
 
 interface TopMenuItemsProps {
@@ -11,13 +12,13 @@ interface TopMenuItemsProps {
 export default function TopMenuItems({ isOpen, closeMenu }: TopMenuItemsProps): JSX.Element {
 
     const menuItems = [
-        { href: "/", label: "Homepage" },
         { href: "/why", label: "Why" },
         { href: "/document", label: "Documents" },
     ];
 
     return (
         <ul className={`flex flex-col sm:flex-row items-center justify-center h-full sm:h-auto pt-16 sm:pt-0 sm:gap-x-4 w-full ${isOpen ? 'flex' : 'hidden sm:flex'}`}>
+            <li className="cursor-default font-semibold mb-2 sm:mb-0 text-xl sm:text-lg"><Link href="/" onClick={closeMenu}>Text Review</Link></li>
             { menuItems.map(item => (
                 <TopMenuItem key={item.href} href={item.href} onClick={closeMenu}>{item.label}</TopMenuItem>
             ))}
