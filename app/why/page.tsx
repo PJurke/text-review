65714dac44891f04a0c482b9122bc6541aaa7b68
Philip Jurke <pjurke@icloud.com>
2025-07-11 18:22:14 +0200

    headlines and spacing

--- app/why/page.tsx
+++ app/why/page.tsx
@@ -6,21 +6,27 @@ export const metadata: Metadata = {
 
 export default function WhyPage(): JSX.Element {
     return (
-        <section className="max-w-[50ch] md:max-w-[75ch] mx-auto p-4 text-wrap transition-[max-width]">
-            <h1 className="text-3xl">Why</h1>
+        <section className="max-w-[50ch] md:max-w-[75ch] mx-auto px-4 my-8 text-wrap transition-[max-width]">
+            <h1 className="text-3xl mb-8">Why</h1>
 
-            <p className="leading-9 mt-8 text-lg">
+            <h2 className="text-xl mb-2">My Mission</h2>
+
+            <p className="leading-relaxed mb-8 text-lg">
                 I believe that everyone should have direct access to important information
                 and the opportunity to understand it in depth - regardless of language barriers.
             </p>
 
-            <p className="leading-9 mt-8 text-lg">
+            <h2 className="text-xl mb-2">My Approach</h2>
+
+            <p className="leading-relaxed mb-8 text-lg">
                 That&apos;s why I centralize texts from various official sources in one place for you,
                 have them automatically translated into your language and provide you with
                 intuitive tools with which you can access the content in a focused and individual way.
             </p>
 
-            <p className="leading-9 mt-8 text-lg">
+            <h2 className="text-xl mb-2">My Goal</h2>
+
+            <p className="leading-relaxed mb-8 text-lg">
                 With my application, I would like to enable you to gain clarity about a variety of texts,
                 to analyze them with focus and in peace, and thus to form your own well-founded opinion.
                 This is my contribution to a global and networked knowledge society.
