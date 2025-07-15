fdc40e73f2d33701a9081b79707189732860b921
Philip Jurke <pjurke@icloud.com>
2025-07-15 20:55:58 +0200

    adapt listTextDocuments

--- services/list-text-documents/business-logic/list-text-documents-logic.ts
+++ services/list-text-documents/business-logic/list-text-documents-logic.ts
@@ -1,22 +1,27 @@
-import { getMongoDb } from "@/app/lib/mongo/mongodb";
-import TextDocumentSummaryEntity from "@/entities/TextDocumentSummaryEntity";
 import logger from "@/lib/logger";
 import prisma from "@/lib/prisma";
-import { DatabaseError } from "@/services/shared/errors/DatabaseError";
-import { mapTextDocumentSummaryEntityToTextDocumentSummary } from "@/shared/TextDocumentSummaryMapper";
+
 import { TextDocumentSummary } from "@/types/TextDocumentSummary";
-import { MongoError } from "mongodb";
 
 export default async function listTextDocuments(): Promise<TextDocumentSummary[]> {
 
-    const textDocuments = await prisma.textDocument.findMany({
-        select: {
-            id: true,
-            title: true,
-            author: true
-        }
-    });
+    try {
 
-    return textDocuments;
+        const textDocuments = await prisma.textDocument.findMany({
+            select: {
+                id: true,
+                title: true,
+                author: true
+            }
+        });
+
+        return textDocuments;
+
+    } catch (error) {
+
+        logger.error('list-text-documents-logic.ts:', error);
+        throw error;
+
+    }
 
 }
