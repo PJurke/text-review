1cda1c3d1264af8bc1331a955dd41127c2a822c0
Philip Jurke <pjurke@icloud.com>
2025-07-21 21:37:20 +0200

    add loki support

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,4 +1,7 @@
 import { createLogger, format, transports } from 'winston';
+import LokiTransport from 'winston-loki';
+
+const env = process.env.NODE_ENV || 'development';
 
 const logger = createLogger({
     level: process.env.LOG_LEVEL || 'info',
@@ -8,8 +11,25 @@ const logger = createLogger({
         format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`)
     ),
     transports: [
-        new transports.Console()
+        new transports.Console(),
+        new LokiTransport({
+            host: process.env.LOKI_HOST || '',
+
+            labels: {
+                app: 'text-review',
+                env: env
+            },
+            json: true,
+            
+            basicAuth: `${process.env.LOKI_USERNAME}:${process.env.LOKI_API_KEY}`,
+
+            onConnectionError: (err) => console.error(err),
+        })
     ]
 });
+
+// No Loki transport other than in production
+if (env !== 'production')
+  logger.remove(logger.transports[1]);
 
 export default logger;
