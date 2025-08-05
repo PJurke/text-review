b71b8a0ce5bfb19edb991ecce107dffafac4b0e6
Philip Jurke <pjurke@icloud.com>
2025-08-05 22:26:54 +0200

    make textdocument strict

--- services/text-documents/text-document.model.ts
+++ services/text-documents/text-document.model.ts
@@ -3,14 +3,30 @@ import { z } from 'zod'
 import { TextDocumentType } from '@prisma/client';
 import { ParagraphSchema } from "./paragraph.model"
 
-export const TextDocumentSchema = z.object({
+export const BaseTextDocumentSchema = z.object({
     id: z.string().oid('The id of a text document must be a valid object id'),
     title: z.string(),
     author: z.string(),
     paragraphs: z.array(ParagraphSchema),
-
-    type: z.nativeEnum(TextDocumentType).optional().nullable(),
-    meta: z.any().optional().nullable(),
 });
 
-export type TextDocument = z.infer<typeof TextDocumentSchema>
+const SpeechTextDocumentSchema = BaseTextDocumentSchema.extend({
+    type: z.literal(TextDocumentType.SPEECH),
+    meta: z.object({
+        date: z.string().datetime().optional(),
+        location: z.string().optional(),
+        occasion: z.string().optional(),
+    }).nullable()
+});
+
+const UnknownTextDocumentSchema = BaseTextDocumentSchema.extend({
+    type: z.literal(TextDocumentType.UNKNOWN),
+    meta: z.null()
+});
+
+export const TextDocumentSchema = z.discriminatedUnion('type', [
+    SpeechTextDocumentSchema,
+    UnknownTextDocumentSchema
+]);
+
+export type TextDocument = z.infer<typeof TextDocumentSchema>;
