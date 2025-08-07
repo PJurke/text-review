5e1fd17653f7892c4c317a2f849841c1a9cef606
Philip Jurke <pjurke@icloud.com>
2025-08-07 15:20:15 +0200

    adapt item styling

--- services/text-documents/ui/components/TextDocumentItem.tsx
+++ services/text-documents/ui/components/TextDocumentItem.tsx
@@ -10,7 +10,7 @@ export type TextDocumentItemProps = {
 export default function TextDocumentItem({ id, title, author }: TextDocumentItemProps): JSX.Element {
     return (
 
-        <Link className="block p-4 rounded-lg hover:bg-gray-50" href={`/document/${id}`}>
+        <Link className="block duration-150 p-4 rounded-lg hover:bg-gray-100 transition-colors" href={`/document/${id}`}>
             <h2 className="mb-0.5 text-xl">{title}</h2>
             <Show when={!!author}>
                 <div className="text-gray-600 text-sm">by {author}</div>
