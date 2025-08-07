b4ae5214f80bc13c635fb094c23d0353028405f8
Philip Jurke <pjurke@icloud.com>
2025-08-07 15:37:51 +0200

    adapt signin page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -39,7 +39,7 @@ export default async function SignInPage(): Promise<JSX.Element> {
             <form action={signInWithGoogle}>
                 <h1 className="text-3xl mb-4">Sign in</h1>
 
-                <button className="border-2 cursor-pointer flex gap-2 items-center justify-center p-2 rounded text-center w-full" type="submit">
+                <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold cursor-pointer flex gap-2 items-center justify-center p-2 rounded text-center transition-colors w-full" type="submit">
                     <GoogleIcon />
                     <span>Sign in with Google</span>
                 </button>
