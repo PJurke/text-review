e8e6ef541182d14c72e6a305adf11b96656ce5e0
Philip Jurke <pjurke@icloud.com>
2025-08-07 15:02:42 +0200

    adapt homepage

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,7 +1,7 @@
 export default function HeroSection(): JSX.Element {
     return (
         <section className="flex flex-col items-center justify-center min-h-[70vh] px-4 text-center">
-            <h1 className="font-light mb-2 text-5xl">Text Review</h1>
+            <h1 className="mb-2 text-5xl">Text Review</h1>
             <p className="leading-relaxed text-gray-500 text-xl">
                 Language shapes our interactions.<br />
                 Texts carry meaning.
--- app/_components/PurposeSection.tsx
+++ app/_components/PurposeSection.tsx
@@ -8,7 +8,7 @@ export default function PurposeSection(): JSX.Element {
                 Analyze complex texts in a focused way and at your own pace.
                 Gain clarity and form an opinion that is truly your own.
             </p>
-            <Link className="text-gray-500" href="/why">Read more about the why</Link>
+            <Link className="text-gray-500 underline hover:text-gray-800" href="/why">Read more about the why</Link>
         </section>
     );
 }
